85ce88633290798816f9b02b14ef1c79671c3c19
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2025-11-11 13:34:28 +0000

    Create cmd.py

---
+++ python/cmd.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/run")
+def run():
+    # WARNING: vulnerable to command injection
+    cmd = request.args.get("cmd", "ls")
+    output = os.popen(cmd).read()
+    return "<pre>{}</pre>".format(output)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
