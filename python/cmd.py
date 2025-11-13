cd681da55a0d65bec76c428bfad1462ab88f981f
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2025-11-13 15:31:57 +0000

    Update cmd.py

--- python/cmd.py
+++ python/cmd.py
@@ -1,24 +1,15 @@
-# from flask import Flask, request
-# import os
+from flask import Flask, request
+import os
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/run")
-# def run():
-#     # WARNING: vulnerable to command injection
-#     cmd = request.args.get("cmd", "ls")
-#     output = os.popen(cmd).read()
-#     return "<pre>{}</pre>".format(output)
+@app.route("/run")
+def run():
+    # WARNING: vulnerable to command injection
+    cmd = request.args.get("cmd", "ls")
+    output = os.popen(cmd).read()
+    return "<pre>{}</pre>".format(output)
 
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5001)
-def run_command(command):
-    
-    os.system(f"ping {command}")  
-    
 if __name__ == "__main__":
-    user_input = input("Enter the user ID: ")
-    print(get_user_info(user_input))
+    app.run(debug=True, port=5001)
 
-    command_input = input("Enter the IP Address to ping: ")
-    run_command(command_input)
