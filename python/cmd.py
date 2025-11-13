a54ffc92a9910f146e767cf8fe5cdfb8583fc0b1
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2025-11-13 15:31:22 +0000

    Update cmd.py

--- python/cmd.py
+++ python/cmd.py
@@ -12,3 +12,13 @@
 
 # if __name__ == "__main__":
 #     app.run(debug=True, port=5001)
+def run_command(command):
+    
+    os.system(f"ping {command}")  
+    
+if __name__ == "__main__":
+    user_input = input("Enter the user ID: ")
+    print(get_user_info(user_input))
+
+    command_input = input("Enter the IP Address to ping: ")
+    run_command(command_input)
