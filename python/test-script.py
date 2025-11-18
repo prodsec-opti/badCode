80c4fc474075012e3e5f2f06dbc3c769237a8bfc
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2025-11-18 18:16:04 +0000

    Update test-script.py

--- python/test-script.py
+++ python/test-script.py
@@ -7,4 +7,20 @@ def get_user(username):
     cursor.execute(query)
     return cursor.fetchall()
 
+def ping_host(host):
+    import os
+    os.system("ping -c 1 " + host)  
 
+    
+@app.route("/user")
+def user():
+    init_db()
+    username = request.args.get("username", "")
+    # WARNING: vulnerable to SQL injection
+    query = "SELECT id, username FROM users WHERE username = '%s'" % username
+    conn = sqlite3.connect(DB)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    row = cursor.fetchone()
+    conn.close()
+    return str(row)
