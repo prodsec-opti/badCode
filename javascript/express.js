4e2061ae79cd3c7e080b96be1977db510adabe15
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2026-01-20 09:06:58 +0000

    Update express.js

--- javascript/express.js
+++ javascript/express.js
@@ -1,50 +1,50 @@
-// const express = require('express')
-// const axios = require('axios');
-// import qs from 'qs';
-// import {baz} from './consts.js';
+const express = require('express')
+const axios = require('axios');
+import qs from 'qs';
+import {baz} from './consts.js';
 
-// const data = { 'bar': 123 };
-// const options = {
-//   method: 'GET',
-//   headers: { 'content-type': 'application/x-www-form-urlencoded' },
-//   data: qs.stringify(data),
-//   url: "http://google.com"
-// };
-// axios(options);
+const data = { 'bar': 123 };
+const options = {
+  method: 'GET',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data: qs.stringify(data),
+  url: "http://google.com"
+};
+axios(options);
 
-// express.csrf();
-// express.methodOverride();
-// const express = express()
+express.csrf();
+express.methodOverride();
+const express = express()
 
-// // GET random number
-// express.get("/random", (req, res) => {
-//   var randomishNumber = crypto.pseudoRandomBytes
-//   res.send(randomishNumber);
-// });
+// GET random number
+express.get("/random", (req, res) => {
+  var randomishNumber = crypto.pseudoRandomBytes
+  res.send(randomishNumber);
+});
 
-// express.get("/", (req, res) => res.send("Hello World!"));
+express.get("/", (req, res) => res.send("Hello World!"));
 
-// express.listen(1000, () => console.log("Server listening on port 1000!"));
+express.listen(1000, () => console.log("Server listening on port 1000!"));
 
-// import axios from 'axios';
+import axios from 'axios';
 
-// async function doGetRequest() {
+async function doGetRequest() {
 
-//   let res = await axios.post('http://google.com');
+  let res = await axios.post('http://google.com');
 
-//   let data = res.data;
-//   console.log(data);
-// }
+  let data = res.data;
+  console.log(data);
+}
 
-// doGetRequest();
+doGetRequest();
 
-// const app = express();
+const app = express();
 
-// app.get('/user', (req, res) => {
-//   const userId = req.query.id;
-//   baz(userId,0)
-//   res.send(`User ID: ${userId}`);
-// });
+app.get('/user', (req, res) => {
+  const userId = req.query.id;
+  baz(userId,0)
+  res.send(`User ID: ${userId}`);
+});
 
 
 
