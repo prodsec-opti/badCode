e47b301876ef60a57d536fd9286b5d3f6117cb81
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2026-01-20 09:18:18 +0000

    Update vuln-code.js

--- javascript/vuln-code.js
+++ javascript/vuln-code.js
@@ -4,24 +4,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-// Vulnerable endpoint
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-// sample changes
-    //Vulnerable query 
-    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+// // Vulnerable endpoint
+// app.post('/login', (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+// // sample changes
+//     const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'testdb',
-    });
+//     const connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'testdb',
+//     });
 
-    connection.query(query, (err, results) => {
-       //to do ...
-    });
+//     connection.query(query, (err, results) => {
+//        //to do ...
+//     });
 
 });
 
