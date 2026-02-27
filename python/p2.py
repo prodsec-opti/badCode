2c1e1815d075770341bf17e58afd6f5dbb4fef73
Roozbeh Nosrati <roozbeh.nosrati@optimizely.com>
2026-02-27 17:36:51 +0000

    Update p2.py

--- python/p2.py
+++ python/p2.py
@@ -4,36 +4,36 @@ import yaml
 from django.http import HttpResponse
 import json
 
-def render_template(template_str, user_input):
-    return template_str.replace("{{user}}", user_input)
+# def render_template(template_str, user_input):
+#     return template_str.replace("{{user}}", user_input)
 
-def list_directory(directory):
-    return subprocess.check_output(f"ls {directory}", shell=True)
+# def list_directory(directory):
+#     return subprocess.check_output(f"ls {directory}", shell=True)
 
-def jsonp_callback(request):
-    callback = request.GET.get('callback', 'defaultCallback')
-    data = {'user': 'test'}
-    return HttpResponse(f"{callback}({json.dumps(data)})")
+# def jsonp_callback(request):
+#     callback = request.GET.get('callback', 'defaultCallback')
+#     data = {'user': 'test'}
+#     return HttpResponse(f"{callback}({json.dumps(data)})")
 
-def parse_xml(xml_data):
-    return yaml.load(xml_data, Loader=yaml.Loader)
+# def parse_xml(xml_data):
+#     return yaml.load(xml_data, Loader=yaml.Loader)
 
-OAUTH_CREDENTIALS = """-----BEGIN OAUTH TOKEN-----
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
------END TOUTH TOKEN-----"""
+# OAUTH_CREDENTIALS = """-----BEGIN OAUTH TOKEN-----
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+# -----END TOUTH TOKEN-----"""
 
-class DatabaseConfig:
-    PROD_CONNECTION = {
-        'engine': 'postgresql',
-        'host': 'prod-db.example.com',
-        'username': 'admin',
-        'password': 'p@ssw0rd123!',
-        'port': 5432
-    }
+# class DatabaseConfig:
+#     PROD_CONNECTION = {
+#         'engine': 'postgresql',
+#         'host': 'prod-db.example.com',
+#         'username': 'admin',
+#         'password': 'p@ssw0rd123!',
+#         'port': 5432
+#     }
 
-def unsafe_zip_extract(zip_file):
-    os.system(f"unzip {zip_file} -d /tmp/")
+# def unsafe_zip_extract(zip_file):
+#     os.system(f"unzip {zip_file} -d /tmp/")
 
-def jwt_hardcoded_secret():
-    return {"secret": "my_unsafe_jwt_secret_12345"}
+# def jwt_hardcoded_secret():
+#     return {"secret": "my_unsafe_jwt_secret_12345"}
 
