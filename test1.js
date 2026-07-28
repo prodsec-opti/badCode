1db3706e2a409eb51ead1949cbe259a011c2c255
Roozbeh Nosrati <167541056+RZB1088@users.noreply.github.com>
2026-07-28 13:55:38 +0100

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,25 @@
+(function() {
+    var lastHeight = 0;
+
+    window.sendCurrentWindowHeightToParent = function() {
+        var newHeight = document.getElementById('content').offsetHeight;
+        if (newHeight !== lastHeight) {
+            lastHeight = newHeight;
+            // window.parent.postMessage (height, "*");
+            window.parent.postMessage(
+                JSON.stringify(
+                    {
+                        source:'optiIFrame',
+                        height: lastHeight
+                    }
+                ), "*");
+        }
+    };
+
+    window.addEventListener("DOMContentLoaded", function () {
+        sendCurrentWindowHeightToParent();
+    });
+    window.addEventListener("resize", function () {
+        sendCurrentWindowHeightToParent();
+    });
+})();
